475f51cdeb7a72bb7020650a6af78a9ddc2cc3e6
Chhanda Naskar <chhandanaskar.official@gmail.com>
2023-05-01 22:47:48 +0530

    jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime   
 
 # Create your models here.
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    updated_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
